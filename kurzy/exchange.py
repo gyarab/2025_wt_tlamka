798af77073dcfdf0542033b9f0a0c7b899084a39
Matouš Tlamka <matous.tlamka.s@gyarab.cz>
2026-01-23 09:07:22 +0100

    Update exchange.py
    Oprava chyby

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,1 +1,7 @@
-httpx
+import httpx
+
+r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
+print(r.text)
+print(r.status)
+lines = r.text.split('\n')
+print(lines[0])
